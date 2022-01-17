46f7a0ce7c2794efdd49cf1bf6b8a5f1d45ece8c
Siddhesh Zantye <siddheshzantye@Siddheshs-MacBook-Pro.local>
2022-01-18 11:17:29 +1300

    Commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CustomDiscordPresence
 
 Rick roll presence on discord
+
+The script connects to discord and every 15 seconds it loops through the lryics of never gonna give you up
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from pypresence import Presence
 import time
 
+start_time = time.time()
 client_id = "931833854069714974"
 rpc = Presence(client_id)
 rpc.connect()
-rpc.update(state="rick rolling you", large_image="rr", buttons=[{"label":"not a rick roll", "url":"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}], start=time.time())
+rpc.update(state="rick rolling you", large_image="rr", small_image="fs", buttons=[{"label":"not a rick roll", "url":"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"label":"my shitty bot", "url":"https://discord.com/api/oauth2/authorize?client_id=896932646846885898&permissions=8&scope=bot%20applications.commands"}], start=time.time())
+
+data = open('/Users/siddheshzantye/Desktop/Coding/Python/Presence/lyrics.txt', 'r')
 
 while True:
-    pass
+    for i in data:
+        time.sleep(15)
+        rpc.update(state=f"{i}", large_image="rr", small_image="fs", buttons=[{"label":"not a rick roll", "url":"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"label":"my shitty bot", "url":"https://discord.com/api/oauth2/authorize?client_id=896932646846885898&permissions=8&scope=bot%20applications.commands"}], start=start_time)
